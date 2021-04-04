5c3030f51f155ac4644dc4705c8eba2842384e82
soohyunnn <sksmsqodn20@gmail.com>
2021-04-04 11:25:55 +0900

    [BE]JPA 프로그래밍 3. Value 타입 맵핑

--- src/main/java/com/springboot/jpa/account/Account.java
+++ src/main/java/com/springboot/jpa/account/Account.java
@@ -20,6 +20,15 @@ public class Account {
     @Transient
     private String no;
 
+    //아래 어노테이션을 생략하면 street으로 필드명이 생성됨.
+    //@AttributeOverrides 사용안할 경우 => city | state | street | zip_code
+    //@AttributeOverrides 사용할 경우 => city | state | home_street | zip_cod
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "street", column = @Column(name = "home_street"))
+    })
+    private Address address;
+
     public Long getId() {
         return id;
     }
---
+++ src/main/java/com/springboot/jpa/account/Address.java
@@ -0,0 +1,13 @@
+package com.springboot.jpa.account;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+
+}
