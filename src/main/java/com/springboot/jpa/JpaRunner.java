c4a9e0e1647557ee68334280aaacc51016cc73eb
soohyunnn <sksmsqodn20@gmail.com>
2021-04-05 20:57:28 +0900

    [BE]JPA 프로그래밍 5. 엔티티 상태와 Cascade

--- src/main/java/com/springboot/jpa/JpaRunner.java
+++ src/main/java/com/springboot/jpa/JpaRunner.java
@@ -1,6 +1,8 @@
 package com.springboot.jpa;
 
 import com.springboot.jpa.account.Account;
+import com.springboot.jpa.account.Comment;
+import com.springboot.jpa.account.Post;
 import com.springboot.jpa.account.Study;
 import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
@@ -20,28 +22,46 @@ public class JpaRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Account account = new Account();
-        account.setUsername("soohyun");
-        account.setPassword("123");
-
-        Study study = new Study();
-        study.setName("Spring Data JPA");
+//        Account account = new Account();
+//        account.setUsername("soohyun");
+//        account.setPassword("123");
+//
+//        Study study = new Study();
+//        study.setName("Spring Data JPA");
 
 
 //        account.getStudies().add(study);  //이거는 Account에 관계를 설정하는 것
 //        study.setOwner(account);  //Study에 관계를 설정하는 것
 
         //위의 두 소스를 하나의 메소드로 리팩토링
-        account.addStudy(study);
+        //account.addStudy(study);
 
         //영속성 -> DB에 저장을 의미
         //entityManager.persist(account);
 
+        Post post = new Post();
+        post.setTitle("Spring Data JPA 언제 보나...");
+
+        Comment comment = new Comment();
+        comment.setComment("빨리 보고 싶어요...");
+        post.addComment(comment);
+
+        Comment comment1 = new Comment();
+        comment1.setComment("곧 보여드릴게요!");
+        post.addComment(comment1);
 
         //JPA는 하이버네이트를 사용하기 때문에 하이버네이트 API를 사용할 수 있습니다.
         //하이버네이트의 가장 핵심적인 API는 session 입니다.
         Session session = entityManager.unwrap(Session.class);
-        session.save(account);
-        session.save(study);
+        //이렇게 하면 Post만 들어갑니다.
+        session.save(post);
+//        session.save(account);
+//        session.save(study);
+
+//        Account soohyun = session.load(Account.class, account.getId());
+//        soohyun.setUsername("SOO");
+//        System.out.println("====================");
+//        System.out.println(soohyun.getUsername());
+        //session.delete(post);
     }
 }
---
+++ src/main/java/com/springboot/jpa/account/Post.java
@@ -0,0 +1,50 @@
+package com.springboot.jpa.account;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Post {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    //CascadeType.PERSIST는 Post의 내용을 Comment에 전파를 해달라고 하는 명령어
+    @OneToMany(mappedBy = "post")
+    private Set<Comment> comments = new HashSet<>();
+
+    public void addComment(Comment comment) {
+        this.getComments().add(comment);
+        comment.setPost(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
+}
