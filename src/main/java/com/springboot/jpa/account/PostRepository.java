4a20c707aadad900241b2db8fa1ce83453400dd7
soohyunnn <sksmsqodn20@gmail.com>
2021-04-06 21:33:59 +0900

    [BE]스프링 데이터 Common 1. Repository(리포지토리)

--- src/main/java/com/springboot/jpa/account/PostRepository.java
+++ src/main/java/com/springboot/jpa/account/PostRepository.java
@@ -1,9 +1,14 @@
 package com.springboot.jpa.account;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    Page<Post> findByTitleContains(String title, Pageable pageable);
+
+    long countByTitleContains(String title);
 
 }
