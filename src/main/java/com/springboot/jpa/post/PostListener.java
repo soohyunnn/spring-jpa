050823498cc7b87ab9af0e97c5517ac210777b9b
soohyunnn <sksmsqodn20@gmail.com>
2021-04-07 23:13:15 +0900

    [BE]스프링 데이터 Common 9. 도메인 이벤트

---
+++ src/main/java/com/springboot/jpa/post/PostListener.java
@@ -0,0 +1,13 @@
+package com.springboot.jpa.post;
+
+import org.springframework.context.ApplicationListener;
+
+public class PostListener implements ApplicationListener<PostPublishedEvent> {
+
+    @Override
+    public void onApplicationEvent(PostPublishedEvent event) {
+        System.out.println("==========================");
+        System.out.println(event.getPost().getTitle() + " is published!!");
+        System.out.println("==========================");
+    }
+}
--- src/test/java/com/springboot/jpa/post/PostRepositoryTest.java
+++ src/test/java/com/springboot/jpa/post/PostRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -14,6 +15,17 @@ public class PostRepositoryTest {
 
     @Autowired
     PostRepository postRepository;
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    public void event() {
+        Post post = new Post();
+        post.setTitle("event");
+        PostPublishedEvent event = new PostPublishedEvent(post);
+        applicationContext.publishEvent(event);
+    }
 
     @Test
     public void crud() {
