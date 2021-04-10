2e8fe3a65002710ff4d44084e87b0cfde5fb1f90
soohyunnn <sksmsqodn20@gmail.com>
2021-04-10 09:01:32 +0900

    [BE]스프링 데이터 JPA 3. JPA 쿼리 메소드 - @Query 사용

--- src/main/java/com/springboot/jpa/post1/Post1.java
+++ src/main/java/com/springboot/jpa/post1/Post1.java
@@ -4,7 +4,6 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@NamedQuery(name = "Post.findByTitle", query = "SELECT p FROM Post1 p WHERE p.title=?1")
 public class Post1 {
 
     @Id @GeneratedValue
--- src/main/java/com/springboot/jpa/post1/Post1Repository.java
+++ src/main/java/com/springboot/jpa/post1/Post1Repository.java
@@ -1,12 +1,13 @@
 package com.springboot.jpa.post1;
 
-import com.springboot.jpa.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface Post1Repository extends JpaRepository<Post1, Long> {
     List<Post1> findByTitleStartsWith(String title);
 
+    @Query("SELECT p FROM Post1 p WHERE p.title=?1")
     List<Post1> findByTitle(String title);
 }
