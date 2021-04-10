b1a016885207aa76d9b8c684ac4232e364913527
soohyunnn <sksmsqodn20@gmail.com>
2021-04-10 10:45:59 +0900

    [BE]스프링 데이터 JPA 5. Named Parameter과 SpEL

--- src/main/java/com/springboot/jpa/post1/Post1Repository.java
+++ src/main/java/com/springboot/jpa/post1/Post1Repository.java
@@ -3,12 +3,13 @@ package com.springboot.jpa.post1;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface Post1Repository extends JpaRepository<Post1, Long> {
     List<Post1> findByTitleStartsWith(String title);
 
-    @Query("SELECT p FROM Post1 p WHERE p.title=?1")
-    List<Post1> findByTitle(String title, Sort sort);
+    @Query("SELECT p FROM #{#entityName} AS p WHERE p.title=:title")
+    List<Post1> findByTitle(@Param("title") String title, Sort sort);
 }
